aa7f95b2af62ff13125816fb050bf64d9e86cb88
baroccc <juan.pablo.barona@correounivalle.edu.co>
2022-11-05 08:13:47 -0500

    first Endpoint YK

---
+++ index.js
@@ -0,0 +1,17 @@
+//Crear aplicación de express
+const express = require('express');
+const res = require('express/lib/response');
+//Ejecutar aplicación de express
+const app = express()
+//Definicion del puerto, donde se manipulan las variables de ambiente
+const port = process.env.PORT || 3000;
+
+//Solicitud al endpoint "/yk"
+app.get('/yk',(req,res)=>{
+    res.send('ola comostas')
+})
+
+//Definir el parametro donde la aplicacion escucha
+app.listen(port, () =>{
+    console.log(`Example app listening on port ${port}`)
+})
